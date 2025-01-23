8016f1ffe695627297e011bdc4351b8f141a71f4
Great Dilla <35280192+dillagr@users.noreply.github.com>
2025-01-23 13:21:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import argparse
+import asyncio
 
+from argparse import ArgumentParser, Namespace
 from loguru import logger
 from rich import print
 from rich.traceback import install
+from dotenv import dotenv_values
 
 # save runtime logs to file
 logger.add("runtime.log", level="DEBUG")
 
 # rich.traceback
 install(show_locals=True)
+cfg = {**dotenv_values()}
 
 
 ############################################################
-async def main():
+async def main(params):
     '''The main function'''
     pass
 
 
 
 ############################################################
-def cli_params() -> None:
+def cli_params() -> Namespace:
     '''Get parameters that are passed through from the command line upon execution.'''
 
     parser = argparse.ArgumentParser()
@@ -35,15 +38,16 @@ def cli_params() -> None:
     parser.add_argument("-i", "--input", type=str, required=False,
                         help="""Some input string.""", )
 
-    # args = parser.parse_args(args=["--debug"])  ## within the jupyter notebook
-    args = parser.parse_args()  # this is for production runs
+    # params = parser.parse_args(params=["--debug"])  ## within the jupyter notebook
+    params = parser.parse_args()  # this is for production runs
 
-    if args.debug:
-        logger.debug(f"ARGS: {args}")
+    if params.debug:
+        logger.debug(f"ARGS: {params}")
 
-    return (args)
+    return (params)
 
 
 ############################################################
 if __name__ == "__main__":
-    asyncio.run(main())
+    params = cli_params()
+    asyncio.run(main(params))
